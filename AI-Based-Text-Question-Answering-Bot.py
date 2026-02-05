897d17c2ac2e807495d97a4c57955fd8232176b3
Satyendra Singh <satyendrasinghdpu@gmail.com>
2026-02-04 20:24:21 -0800

    Refactor chatbot title and maintain code structure

--- AI-Based-Text-Question-Answering-Bot.py
+++ AI-Based-Text-Question-Answering-Bot.py
@@ -57,7 +57,7 @@ def answer_question(query):
 if __name__ == "__main__":
     import streamlit as st
 
-st.title("Satyendra's Chatbot")
+st.title("Satyendra's & Shriyansh Chatbot")
 
 user_query = st.text_input("Ask a question:", placeholder="Write your Question Here...")
 
@@ -67,6 +67,7 @@ if user_query:
     st.success(answer)
 
 # %%
+
 
 
 
